e9602800181ef6479b94d0d7f501a9dcd56bfb1c
Erik-NA7 <58936291+Erik-NA7@users.noreply.github.com>
2024-12-18 02:23:14 +0700

    Update firebaseConfig.ts

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -7,12 +7,12 @@ import { initializeApp, FirebaseApp, FirebaseOptions } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDFcCCHWgdOhOFRMkEqm7EN_XFpl2dzQIc",
-  authDomain: "ebuddy-30bc4.firebaseapp.com",
-  projectId: "ebuddy-30bc4",
-  storageBucket: "ebuddy-30bc4.firebasestorage.app",
-  messagingSenderId: "846676233483",
-  appId: "1:846676233483:web:6033c328a86e9bb52ce60e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
